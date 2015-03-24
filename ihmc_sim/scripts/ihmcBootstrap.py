10b018f128bc5b7c18eee76ed8df40d607c79882
Doug Stephen <dstephen@ihmc.us>
2015-03-23 23:17:39 -0500

    Updating sim bootstrap script and some messages.

--- ihmc_sim/scripts/ihmcBootstrap.py
+++ ihmc_sim/scripts/ihmcBootstrap.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
-import tarfile, os, sys, stat
+import tarfile, os, sys, stat, rospkg
 from urllib2 import urlopen, URLError, HTTPError
 
 
 try:
-    distVersion = "IHMCAtlasAPI-0.2.1-alpha"
-    scriptsDir = os.path.dirname(os.path.realpath(__file__))
-    destFile = scriptsDir + "/" + distVersion + ".tar"
+    rospack = rospkg.RosPack()
+    distVersion = "IHMCAtlasAPI-0.3.0-alpha"
+    ihmcSimDir = rospack.get_path('ihmc_sim')
+    distVersionDir = ihmcSimDir + "/" + distVersion
+    destFile = distVersionDir + ".tar"
 
-    if os.path.exists(scriptsDir + "/" + distVersion):
-        print "An API instance already exists, please remove " + scriptsDir + "/" + distVersion + " and run the bootstrapper again."
+    if os.path.exists(distVersionDir):
+        print "An API instance already exists, please remove " + distVersionDir + " and run the bootstrapper again."
         exit(1)
 
-    distTar = urlopen("https://bitbucket.org/ihmcrobotics/ihmc_msgs/downloads/" + distVersion + ".tar")
+    distTar = urlopen("https://bitbucket.org/ihmcrobotics/ihmc_ros/downloads/" + distVersion + ".tar")
     print "Downloading the IHMC Atlas API distribution..."
 
     with open(destFile, "wb") as local_file:
@@ -20,10 +22,10 @@ try:
 
     print "Untarring and cleaning up..."
     archiveHandle = tarfile.TarFile(destFile, "r")
-    archiveHandle.extractall(scriptsDir)
+    archiveHandle.extractall(ihmcSimDir)
     os.remove(destFile)
 
-    binDir = scriptsDir + "/" + distVersion + "/bin"
+    binDir = distVersionDir + "/bin"
     for script in os.listdir(binDir):
         if not script.endswith("Configurations") and not script.endswith(".bat"):
             os.chmod(binDir + "/" + script, stat.S_IRWXU | stat.S_IRWXG | stat.S_IROTH)
